84da4910afc5c91f36dafde447907dce391d8f0a
shukratamore <shukrata.qualtech@gmail.com>
2024-04-18 15:43:00 +0530

    update

--- EventAPI/server.js
+++ EventAPI/server.js
@@ -1,27 +1,26 @@
-const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const indexRouter = require("./router.js");
-
+const eventRouter = require("./event.js");
+const categoryRouter = require("./category.js");
+const eventFilesRouter = require("./event-files.js");
+const path = require("path");
 const app = express();
 
+// Middleware
 app.use(express.json());
-
 app.use(bodyParser.json());
-
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
-
 app.use(cors());
 
-app.use("/api", indexRouter);
-
-// Handling Errors
+// Routes
+app.use("/api", eventRouter);
+app.use("/api", categoryRouter);
+app.use("/api", eventFilesRouter);
 app.use((err, req, res, next) => {
   // console.log(err);
   err.statusCode = err.statusCode || 500;
@@ -30,5 +29,8 @@ app.use((err, req, res, next) => {
     message: err.message,
   });
 });
-
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
